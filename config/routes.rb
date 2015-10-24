7096a47da8e70c20ccc90033f3aebbc4f4c00b0e
Altieres Lopes <altiereslopes@webgoal.com.br>
2015-10-24 15:36:05 -0200

    interests scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   root 'learning_propositions#index'
   devise_for :users
-  resources :learning_propositions, only: [:index]
+  resources :learning_propositions, only: [:index] do
+    resources :interests, only: [:new, :create]
+  end
 
 
 end
