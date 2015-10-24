50f85eb43acf01ef084fb6f7a00f26c24eee1fa4
Rafael Valério <rafael@webgoal.com.br>
2015-10-24 16:38:48 -0200

    criar sessao

--- app/controllers/learning_propositions_controller.rb
+++ app/controllers/learning_propositions_controller.rb
@@ -3,7 +3,21 @@ class LearningPropositionsController < ApplicationController
   end
 
   def show
+
   end
+
   def new
+    @learning_proposition = LearningProposition.new
+
+  end
+
+  def create
+    @learning_proposition = LearningProposition.new(learning_proposition_params)
+    @learning_proposition.save
+  end
+
+  private
+  def learning_proposition_params
+    params.require(:learning_proposition).permit(:name, :description, :min_attendees, :max_attendees, :location, :session_date)
   end
 end
