6c73d0c25b111888dcabb225b8cb39866c267033
Guilherme Gonçalves <ggoncalves.kcond@gmail.com>
2015-10-24 16:29:03 -0200

    Create validations to the model LearningPropostion

--- spec/factories/learning_propositions.rb
+++ spec/factories/learning_propositions.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :learning_proposition do
     name "MyString"
-session_date "2015-10-24 16:02:08"
-location "MyString"
-min_attendees 1
-max_attendees 1
+    session_date DateTime.now
+    location "MyString"
+    min_attendees 2
+    max_attendees 5
   end
 
 end
--- spec/models/learning_proposition_spec.rb
+++ spec/models/learning_proposition_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe LearningProposition, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'Should not create Learning Proposition with less than 2 attendees' do 
+    learning_proposition = create(:learning_proposition)
+    learning_proposition.min_attendees = 1
+    expect(learning_proposition.save).to be false
+  end
+  
 end
