d3c85bd234763865267a7c0f56f513050656cd14
Gustavo Vinícius <gustavo@webgoal.com.br>
2015-11-14 15:30:29 -0200

    Layout da index

--- app/models/learning_proposition.rb
+++ app/models/learning_proposition.rb
@@ -1,6 +1,6 @@
 class LearningProposition < ActiveRecord::Base
     has_many :interests
-    validates :name, :session_date, :location, :min_attendees, :max_attendees, :password, presence: true
+    validates :name, :session_date, :location, :min_attendees, :max_attendees, :password, :description, presence: true
     validates :min_attendees, :max_attendees, numericality: { only_integer: true, greater_than_or_equal_to: 2 }
     validates :name, :location, length: {minimum:5}
 
